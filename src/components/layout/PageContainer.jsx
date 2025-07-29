3bf7addc70847c533acc967d400e714243e83279
J1MMM <jimuelbaraero00@gmail.com>
2025-07-29 16:36:17 +0800

    refactor: change height and b-radius for data grid table

--- src/components/layout/PageContainer.jsx
+++ src/components/layout/PageContainer.jsx
@@ -9,7 +9,7 @@ export const PageContainer = ({
 }) => {
   return (
     <Box sx={{ p: 2, boxSizing: "border-box", bgcolor: "#f8fafd" }}>
-      <Box height={`calc(100vh - ${160}px)`} width="100%">
+      <Box height={`calc(100vh - ${170}px)`} width="100%">
         {children}
       </Box>
     </Box>
